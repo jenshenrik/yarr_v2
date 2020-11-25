a4c44e6beeee580e46738d58c00376ec20b65af8
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-11-25 13:40:30 +0100

    Part 6 refactoring done

--- procgen.py
+++ procgen.py
@@ -10,6 +10,7 @@ from game_map import GameMap
 import tile_types
 
 if TYPE_CHECKING:
+    from engine import Engine
     from entity import Entity
 
 
@@ -86,10 +87,12 @@ def generate_dungeon(
     map_width: int,
     map_height: int,
     max_monsters_per_room: int,
+    engine: Engine,
     player: Entity,
 ) -> GameMap:
     """Generate a new dungeon map."""
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
@@ -109,7 +112,7 @@ def generate_dungeon(
 
         if len(rooms) == 0:
             # this is the first room, and the starting point
-            player.x, player.y = new_room.center
+            player.place(*new_room.center, dungeon)
         else:
             # Dig out a tunnel between this room and the previous one
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
