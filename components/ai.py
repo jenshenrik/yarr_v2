dc4eddd976d621d295a743ffbfbec195b71410b9
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-11-27 10:12:33 +0100

    Part 9 done

--- components/ai.py
+++ components/ai.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-from typing import List, Tuple, TYPE_CHECKING
+import random
+from typing import List, Optional, Tuple, TYPE_CHECKING
 
 import numpy as np
 import tcod
 
-from actions import Action, MeleeAction, MovementAction, WaitAction
+from actions import Action, BumpAction, MeleeAction, MovementAction, WaitAction
 
 if TYPE_CHECKING:
     from entity import Entity
@@ -70,3 +71,48 @@ class HostileEnemy(BaseAI):
             ).perform()
 
         return WaitAction(self.entity).perform()
+
+
+class ConfusedEnemy(BaseAI):
+    """
+    A confused enemy will stumble aimlessly for a given number of turns, then revert
+    back to its previous AI. If an actor occupies a tile it is randomly moving into,
+    it will attack.
+    """
+
+    def __init__(
+        self, entity: Actor, previous_ai: Optional[BaseAI], turns_remaining: int
+    ):
+        super().__init__(entity)
+
+        self.previous_ai = previous_ai
+        self.turns_remaining = turns_remaining
+
+    def perform(self) -> None:
+        # Revert the AI back to the original state if the effect has run its course
+        if self.turns_remaining <= 0:
+            self.engine.message_log.add_message(
+                f"The {self.entity.name} is no longer confused."
+            )
+            self.entity.ai = self.previous_ai
+        else:
+            # Pick a random direction
+            direction_x, direction_y = random.choice(
+                [
+                    (-1.0 - 1),  # NW
+                    (0, -1),  # N
+                    (1, -1),  # NE
+                    (-1, 0),  # W
+                    (1, 0),  # E
+                    (-1, 1),  # SW
+                    (0, 1),  # S
+                    (1, 1),  # SE
+                ]
+            )
+
+            self.turns_remaining -= 1
+
+            # The actor will either try to move or attack in the chosen random
+            # direction. Its possible the actor will just bump into a wall, wasting
+            # a turn.
+            return BumpAction(self.entity, direction_x, direction_y,).perform()
